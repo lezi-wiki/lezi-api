f2584eef4def0ac32eb5cab65b9139245dcb6d73
HenryJiu <2487425131@qq.com>
2022-07-07 21:19:17 +0800

    First commit codes.

--- main.go
+++ main.go
@@ -6,11 +6,6 @@ import (
 )
 
 func main() {
-	//if InitJSON == 1 {
-	//	fmt.Println("MySQL database connect error.Please check your MySQL database and config file.")
-	//} else {
-	//	fmt.Printf("MySQL database connect success.Database id:%d\n", InitJSON)
-	//}
 	InitRouter := Router.InitRouter()
 	if InitRouter == 1 {
 		fmt.Println("Router Error!")
