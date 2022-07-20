9ee8daeb7408e3d6b6043e55ec2b334efffc017e
AH-dark <ahdark@outlook.com>
2022-07-20 11:05:53 +0800

    Feat: log display on data update

--- pkg/cron/data_jobs.go
+++ pkg/cron/data_jobs.go
@@ -2,14 +2,20 @@ package cron
 
 import (
 	"github.com/lezi-wiki/lezi-api/pkg/text"
+	"github.com/lezi-wiki/lezi-api/pkg/util"
 	"github.com/lezi-wiki/lezi-api/services/remote"
 )
 
 func getNewData() {
+	util.Log().Info("准备从 GitHub 更新数据集")
 	data, err := remote.GetDataFromGitHub()
 	if err != nil {
+		util.Log().Error("更新数据集失败")
 		return
 	}
 
 	text.Data = data
+	util.Log().Debug("数据集已更新：%v", text.Data)
+
+	util.Log().Info("数据集更新完成")
 }
